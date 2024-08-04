ee1bf20a0a1a97e13be4c5c89bb295843aba9cb9
ali-lutfalla <ali,latfalla@gmail.com>
2024-08-04 14:06:30 +0300

    finish

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,23 @@ import './App.css'
 
 const App = () => {
 
+  const books = [
+    {name:'arthur', red: true},
+    {name:'ali', red: true},
+    {name:'lauther', red: false},
+    {name:'Jones', red: true},
+
+  ]
+
   return (
-    <h1>Hello world!</h1>
+    <>
+    <h1>books:</h1>
+    <ul>
+      {books.map((book, index) => (
+        <li key={index} className= {book.red ? 'completed' : 'not-completed'}>{book.name}</li>
+      ))}
+    </ul>
+    </>
   );
 }
 
